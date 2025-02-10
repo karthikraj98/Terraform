cda6fb2a50f59be182f9c2dac677dd2408d42e01
karthikraj98 <karthikbayanna98@gmail.com>
2025-02-10 16:06:06 +0530

    commit terraform

---
+++ ec2.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "allow_ssh_terraform" {
+    name = "allow_ssh"
+    description = "Allow port number 22 for ssh access"
+
+    #usally  we allow everything in egrees
+    egress {
+         from_port       = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    egress {
+         from_port       = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"] #allow from everyone
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+    Name = "allow_ssh"
+  }
+}
