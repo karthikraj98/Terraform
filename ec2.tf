8ecd74be882ee9f9a1418324ddf450bbe76c3441
karthikraj98 <karthikbayanna98@gmail.com>
2025-02-10 16:35:43 +0530

    commit the changes

--- ec2.tf
+++ ec2.tf
@@ -23,3 +23,23 @@ resource "aws_security_group" "allow_ssh_terraform" {
     Name = "allow_ssh"
   }
 }
+
+resource "aws_instance" "terraform" {
+
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = {
+    Name = "terraform"
+   }
+}
+
+resource "aws_instance" "terraform1" {
+
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = {
+    Name = "terraform1"
+   }
+}
