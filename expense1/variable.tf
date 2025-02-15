39e3990332c137a8c3eb158e36f33ffb1a096977
karthikraj98 <karthikbayanna98@gmail.com>
2025-02-15 21:51:30 +0530

    commit changes

---
+++ expense1/variable.tf
@@ -0,0 +1,21 @@
+variable "instance_names" {
+  type    = list(string)
+  default = ["mysql", "backend", "frontend"]
+}
+
+# variable "common_tags"{
+#     type = map
+#     default = {
+#         Project = "expense"
+#         Enviornment = "Dev"
+#         Terraform = "true"
+#     }
+# }
+
+variable "zone_id" {
+  default = "Z08329612LLVLHI8AGFPS"
+}
+
+variable "domain_name" {
+  default = "learningdevops.shop"
+}
